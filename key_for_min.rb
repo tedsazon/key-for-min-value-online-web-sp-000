1c42e4ee16298af2729a0a343da26c2b952a1c21
Ted Sazon <tedsazon@gmail.com>
2018-12-19 19:21:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.each do |name, numb|
+    if numb < numb.all
+      numb
+    else
+      nil
+    end
+  end
 end
